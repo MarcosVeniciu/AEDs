57909401348036e1d7e033982ed48521c74aaf27
Marcos <marcos.barbalho@ufv.br>
2018-08-23 14:03:28 -0300

    f

--- Exercicios/Lista_1/exercicio_2 (ERRO)/funcao.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-void zerar(int *valor_1, int *valor_2);
--- Exercicios/Lista_1/exercicio_4/rotacao.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-
-void rotacionar(int *d, int *e, int *f);
