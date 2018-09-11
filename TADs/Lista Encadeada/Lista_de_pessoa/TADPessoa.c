d3f5e9ccd797a4bcc3379df2e3853240b0fada16
Marcos <marcos.barbalho@ufv.br>
2018-09-11 17:02:40 -0300

    s

--- TADs/Lista Encadeada/Lista_de_pessoa/TADPessoa.c
+++ TADs/Lista Encadeada/Lista_de_pessoa/TADPessoa.c
@@ -3,10 +3,17 @@
 
 void inicializar_pessoa(TipoPessoa *inicializar_nome){
   inicializar_nome->nome[0] = '\0';
-
-
 }
-int  preencher_pessoa(TipoPessoa *preencher_nome, char *nome);
-void buscar_pessoa(TipoPessoa *buscar_nome, char *nome);
-void remover_pessoa(TipoPessoa *remover_nome, char *nome);
-void impri_pessoa(TipoPessoa *imprimir_nome); 
+
+int  preencher_pessoa(TipoPessoa *preencher_nome, char *nome){
+  strcpy (preencher_nome->nome, nome);
+  return 0;
+}
+
+int buscar_pessoa(TipoPessoa *buscar_nome, char *nome){
+
+
+  return 0;
+}
+int remover_pessoa(TipoPessoa *remover_nome, char *nome);
+void impri_pessoa(TipoPessoa *imprimir_nome);
--- TADs/Lista Encadeada/Lista_de_pessoa/TADPessoa.h
+++ TADs/Lista Encadeada/Lista_de_pessoa/TADPessoa.h
@@ -6,11 +6,10 @@
 typedef struct{
   char nome[31];
 }TipoPessoa;
-a
 
 
 void inicializar_pessoa(TipoPessoa *inicializar_nome);
 int  preencher_pessoa(TipoPessoa *preencher_nome, char *nome);
-void buscar_pessoa(TipoPessoa *buscar_nome, char *nome);
-void remover_pessoa(TipoPessoa *remover_nome, char *nome);
+int buscar_pessoa(TipoPessoa *buscar_nome, char *nome);
+int remover_pessoa(TipoPessoa *remover_nome, char *nome);
 void impri_pessoa(TipoPessoa *imprimir_nome);
